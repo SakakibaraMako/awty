8447cad6e73db85434a95860c7026439b2c61b7a
Mako <dhuw@uw.edu>
2022-02-22 03:25:21 -0800

    part 2, no extra credits

--- app/src/main/java/edu/uw/ischool/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/awty/MainActivity.kt
@@ -6,8 +6,10 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
 import android.view.LayoutInflater
 import android.widget.*
 
@@ -15,32 +17,38 @@ class IntentListener : BroadcastReceiver() {
 
     val MESSAGE_KEY = "Message"
     val PHONE_KEY = "Phone"
-    val captionTemplate = "Texting (%s) %s-%s"
-
     override fun onReceive(context: Context?, intent: Intent?) {
+        val smsManager = SmsManager.getDefault()
         val message = intent!!.getStringExtra(MESSAGE_KEY)
-        val phone = intent!!.getStringExtra(PHONE_KEY)
-        val areaCode = phone!!.substring(0, 3)
-        val prefix = phone!!.substring(3, 6)
-        val lineNumber = phone!!.substring(6)
-
-        val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val activity = context as MainActivity
-        val toastView = activity.findViewById<LinearLayout>(R.id.customToast)
-        var layout = inflater.inflate(R.layout.toast, toastView)
-        var captionLabel = layout.findViewById<TextView>(R.id.tvCaption)
-        var body = layout.findViewById<TextView>(R.id.tvBody)
-        captionLabel.text = String.format(captionTemplate, areaCode, prefix, lineNumber)
-        body.text = message
-
-        with(Toast(context)) {
-            duration = Toast.LENGTH_LONG
-            view = layout
-            show()
-        }
-//        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
-//        Log.i("MainActivity", "Are we there yet?")
+        val destination = intent!!.getStringExtra(PHONE_KEY)
+        smsManager.sendTextMessage(destination, null, message, null, null)
     }
+
+//    override fun onReceive(context: Context?, intent: Intent?) {
+//        val message = intent!!.getStringExtra(MESSAGE_KEY)
+//        val phone = intent!!.getStringExtra(PHONE_KEY)
+//        val areaCode = phone!!.substring(0, 3)
+//        val prefix = phone!!.substring(3, 6)
+//        val lineNumber = phone!!.substring(6)
+//
+//        val inflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//        val activity = context as MainActivity
+//        val toastView = activity.findViewById<LinearLayout>(R.id.customToast)
+//        var layout = inflater.inflate(R.layout.toast, toastView)
+//        var captionLabel = layout.findViewById<TextView>(R.id.tvCaption)
+//        var body = layout.findViewById<TextView>(R.id.tvBody)
+//        captionLabel.text = String.format(captionTemplate, areaCode, prefix, lineNumber)
+//        body.text = message
+//
+//        with(Toast(context)) {
+//            duration = Toast.LENGTH_LONG
+//            view = layout
+//            show()
+//        }
+////        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
+////        Log.i("MainActivity", "Are we there yet?")
+//    }
+
 
 }
 
@@ -59,6 +67,9 @@ class MainActivity : AppCompatActivity() {
     private val missingMessage = "Message can not be empty!"
     private val incorrectPhoneNumber = "Incorrect Phone Number!"
     private val missingTimeInterval = "Missing Time Interval!"
+    private val smsPermission = "android.permission.SEND_SMS"
+    private val smsPermissionRequestCode = 42
+    private val permissionDelied = "Failed to request permission!"
 
     private lateinit var receiver: IntentListener
     private lateinit var intentFilter: IntentFilter
@@ -90,7 +101,13 @@ class MainActivity : AppCompatActivity() {
                     if(hasMessage()) {
                         if(hasPhone()) {
                             if (hasTime()) {
-                                start()
+                                if (checkSelfPermission(smsPermission) ==
+                                    PackageManager.PERMISSION_GRANTED) {
+                                    start()
+                                }
+                                else {
+                                    requestPermissions(arrayOf(smsPermission), smsPermissionRequestCode)
+                                }
                             }else Toast.makeText(this, missingTimeInterval, Toast.LENGTH_LONG).show()
                         }else Toast.makeText(this, incorrectPhoneNumber, Toast.LENGTH_LONG).show()
                     } else Toast.makeText(this, missingMessage, Toast.LENGTH_LONG).show()
@@ -135,6 +152,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        alarmManager.cancel(pendingIntent)
+        if (alarmManager != null) {
+            alarmManager.cancel(pendingIntent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            smsPermissionRequestCode -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    start()
+                }else {
+                    Toast.makeText(this, permissionDelied, Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
     }
 }
